eb611cf313f9154a187b233df75e2da608046b7d
zhanglxlx <zhanglx@tedu.cn>
2019-04-09 15:44:52 +0800

    测试admin

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 /**
  * 小肥牛扫码点餐项目-API子系统
  */
-console.log(new Date().toLocaleString());
 // 创建一个端口
 const PORT=8090;
 // 引入express
 const express=require("express");
 const  cors=require("cors");
+// 中间件 获取post请求的数据
 const bodyParser=require("body-parser");
 const categoryRoutes=require("./routes/admin/category");
+const adminRoutes=require("./routes/admin/admin");
 
 //启动主服务器  创建http应用服务器
 var app=express();
 app.listen(PORT,()=>{console.log("api服务器启动成功啦")});
-app.use(bodyParser.urlencoded({
-    extended:false
-}) );
+//app.use(bodyParser.urlencoded({}))  //把application/x-www-form-urlencoded 格式的请求主题数据解析出来放入req.body属性
+app.use(bodyParser.json() ); //把json格式的请求肢体数据解析出来放入req.body属性
 // 使用中间件,解决跨域问题
 app.use(cors());
 
 // 挂在admin文件
+
 app.use('/admin/category',categoryRoutes);
+app.use('/admin/admin',adminRoutes);
--- routes/admin/category.js
+++ routes/admin/category.js
@@ -7,7 +7,7 @@ const pool=require("../../pool");
 var router=express.Router();
 
 module.exports=router;
-/**
+/**查询菜品
  * API ：GET /admin/category  获取数据
  * 含义：客户端获取所有的彩品类别,按编号升序排列
  * 返回值：[{cid:1,cname:"},{...}]
@@ -15,60 +15,54 @@ module.exports=router;
 router.get("/",(req,res)=>{
     pool.query("SELECT * FROM xfn_category ORDER BY cid",(err,result)=>{
         if(err){throw err};
-        var jsonData=JSON.stringify(result);
-        res.send("doData('+jsonData+')");
+        if(result.affectedRows>0){
+            var jsonData=JSON.stringify(result);
+            res.send("doData('+jsonData+')");
+        }else{
+            res.send({code:400,msg:"查询失败"})
+        }
+
     })
 })
-/**
+/**删除菜品
  * API :DELETE /admin/category/:cid 删除
  * 含义：根据表示菜品编号的路由参数，删除该菜品
  * 返回值：{code:200,msg:"1 category deleted"}
  * 返回值：{code:400,msg:"0 category deleted"}
  */
 router.delete("/:cid",(req,res)=>{
-    if(!req.params.cid){
-        res.send({code:403,msg:"cid is not exists"});
-        return;
-    }
-    pool.query("SELECT * FROM xfn_category WHERE cid=?",req.params.cid,(err,result)=>{
+    //注意：删除菜品类别钱必须先把属于该类别的菜品的类别编号设置为null
+    pool.query("UPDATE xfn_dish SET categoryID=NULL  WHERE categoryID=?",req.params.cid,(err,result)=>{
         if(err){throw err};
-        if(result.affectedRows>0){
-            pool.query("DELETE FROM xfn_category WHERE cid=?",req.params.cid,(err,result)=>{
-                if(err){throw err};
-                if(result.affectedRows>0){
-                    res.send({code:200,msg:"1 category deleted"})
-                }else{
-                    res.send({code:200,msg:"0 category deleted"})
-                }
-            })
-        }else{
-            res.send({code:404,msg:"cid is not exists"});
-        }
+        //至此指定类别的菜品已经修改完毕
+        pool.query("DELETE FROM xfn_category WHERE cid=?",req.params.cid,(err,result)=>{
+            if(err){throw err};
+            if(result.affectedRows>0){
+                res.send({code:200,msg:"1 category deleted"})
+            }else{
+                res.send({code:400,msg:"0 category deleted"})
+            }
+        })
     })
-
 })
-/**
- * API :POST /admin/category   添加
- * 请求参数：请求主体 {cname:""}
+/**添加菜品
+ * API :POST /admin/category   添加 幂等
+ * 请求参数：请求主体 {cname:"xxx"}
  * 含义：添加新的菜品类别
  * 返回值：{code:200,msg:"1 category POST",cid:x}
  */
-router.post("/add",(req,res)=>{
-    var cname=req.body.cname;
-    if(!cname){
-        res.send({code:401,msg:"菜品名称不能为空"});
-        return;
-    }
-    pool.query("INSERT INTO xfn_category VALUES ?",[req.body],(err,result)=>{
+router.post("/",(req,res)=>{
+    pool.query("INSERT INTO xfn_category SET ?",req.body,(err,result)=>{ //注意此处SQL语句的简写
         if(err){throw err};
         if(result.affectedRows>0){
-            res.send({code:200,msg:"1 category add",cid:NULL})
+            res.send({code:200,msg:"1 category added"})
         }else{
-            res.send({code:201,msg:"0 category add"})
+            res.send({code:400,msg:"category added"})
         }
+
     })
 })
-/**
+/**更新菜品
  * API :PUT /admin/category   修改
  * PATCH  (部分修改)
  * 请求参数：请求主体 {cid:x,cname:""}
@@ -77,3 +71,17 @@ router.post("/add",(req,res)=>{
  * 返回值：{code:400,msg:"0 category modified ，not exists"} 类别不存在
  * 返回值：{code:401,msg:"0 category modified no modification"} 修改的名字和以前是一样的
  */
+router.put("/",(req,res)=>{
+    //todo此处可以对数据进行验证
+    pool.query("UPDATE  xfn_category SET ? WHERE cid=?",[req.body,req.body.cid],(err,result)=>{
+        //注意此处SQL语句的简写
+        if(err){throw err};
+        if(result.changedRows>0){  //实际更新了一行
+            res.send({code:200,msg:"1 category modified"})
+        }else  if(result.affectedRows==0){
+            res.send({code:400,msg:"category not exists "})
+        }else if(result.affectedRows==0&&result.affectedRows==0){ //
+            res.send({code:401,msg:"no category modified"})
+        }
+    })
+})
