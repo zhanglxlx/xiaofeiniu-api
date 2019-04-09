aa655ddaff2058e03387f4f74041840c11a0dee2
zhanglxlx <zhanglx@tedu.cn>
2019-04-09 14:16:20 +0800

    测试admin

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -2,9 +2,34 @@
  * 用户管理路由
  */
 const express=require("express");
-const pool=require("../../pool.js");
+const pool=require("../../pool");
 var router=express.Router();
-router.get("/test",(req,res)=>{
+/**get 请求可以有主体吗？
+ * API:get /admin/login
+ * 请求数据 {aname:"xxx",apwd:"123456"}
+ * 完成用户登录验证
+ * 返回数据：
+ * {code:200,msg:"login  success"}
+ * {code:400,msg:"aname or apwd err"}
+ */
+router.get("/login",(req,res)=>{
+    pool.query("SELECT * FROM xfn_admin WHERE aname=? AND apwd=?",[req.body.aname,req.body.apwd],(err,result)=>{
+        if(err) {throw err};
+        if(result.affectedRows>0){
+            res.send({code:200,msg:"login success"})
+        }else{
+            res.send({code:400,msg:"aname or apwd err"})
+        }
+    })
     console.log(1);
 })
-module.express=router;
+
+/**get 请求可以有主体吗？
+ * API:PATCH /admin/login
+ * 请求数据 {aname:"xxx",apwd:"123456"}
+ * 根据管理员用户名和密码修改管理员密码
+ * 返回数据：
+ * {code:200,msg:"modified succ"}
+ * {code:400,msg:"aname or apwd err"}
+ */
+module.exports=router;
